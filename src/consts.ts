678c28575933ec508c41b5ead2adcd97b13ceb7e
unyousan69 <294409208+unyousan69@users.noreply.github.com>
2026-09-01 22:08:36 +0900

    Initial blog setup

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '29歳、無職。今日からブログを始めます。';
+export const SITE_DESCRIPTION = '29歳、無職。日々の生活や挑戦したことを記録していくブログです。';
